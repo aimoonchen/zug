d155d803f63fcef9dcbf24ef49219a122b7d968e
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2026-01-11 20:35:25 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     nix-github-actions = {
       url = "github:nix-community/nix-github-actions";
